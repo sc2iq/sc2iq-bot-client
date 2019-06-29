6e5a691e93208f612ea59b978f69dc1bf311814d
Matt Mazzola <mattmazzola@live.com>
2019-06-29 13:13:36 -0700

    get working chat

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,59 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import './App.css'
+
+interface IMessage {
+  value: string
+}
 
 const App: React.FC = () => {
+  const [input, setInput] = React.useState('')
+  const [messages, setMessages] = React.useState<IMessage[]>([])
+  const addMessge = (m: string) => {
+    const newMessage: IMessage = {
+      value: m
+    }
+    // Add message
+    setMessages(msgs => {
+      msgs.push(newMessage)
+      return msgs
+    })
+    // Clear input
+    setInput('')
+  }
+
+  const onChangeInput = (event: React.ChangeEvent<HTMLInputElement>) => {
+    console.log(`onChangeInput`, { event })
+    const text = event.target.value
+    setInput(text)
+  }
+
+  const onKeyDownInput = (event: React.KeyboardEvent<HTMLInputElement>) => {
+    console.log(`onKeyDownInput`, { event })
+    switch (event.key) {
+      case 'Enter': {
+        addMessge(input)
+      }
+    }
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className="app">
+      <header className="app-header">
+        <h2>SC2IQ Bot Client</h2>
       </header>
+      <div className="chat">
+        <div className="chat-messages">
+
+          {messages.map((m, i) =>
+            <div key={i} className="chat-message">
+              Message: {m.value}
+            </div>
+          )}
+        </div>
+        <div className="chat-input">
+          <input type="text" value={input} placeholder="Type your message..." onChange={onChangeInput} onKeyDown={onKeyDownInput} />
+        </div>
+      </div>
     </div>
   );
 }
