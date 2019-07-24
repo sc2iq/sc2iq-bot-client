5baa3112882a5c4addf06886a852b088d3ae0729
Matt Mazzola <mattmazzola@live.com>
2019-07-23 19:59:57 -0700

    Fix webchat connection and add styling from tutorials

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,133 @@ import React from 'react'
 import './App.css'
 import WebChat, { createDirectLine } from 'botframework-webchat'
 
+/**
+const DEFAULT_OPTIONS = {
+  // Color and paddings
+  accent: DEFAULT_ACCENT,
+  backgroundColor: 'White',
+  cardEmphasisBackgroundColor: '#F0F0F0',
+  paddingRegular: PADDING_REGULAR,
+  paddingWide: PADDING_REGULAR * 2,
+  subtle: DEFAULT_SUBTLE,
+
+  // Word break
+  messageActivityWordBreak: 'break-word', // 'normal' || 'break-all' || 'break-word' || 'keep-all'
+
+  // Fonts
+  fontSizeSmall: '80%',
+  monospaceFont: fontFamily(['Consolas', 'Courier New', 'monospace']),
+  primaryFont: fontFamily(['Calibri', 'Helvetica Neue', 'Arial', 'sans-serif']),
+
+  // Avatar
+  avatarSize: 40,
+  botAvatarImage: '',
+  botAvatarInitials: '',
+  userAvatarImage: '',
+  userAvatarInitials: '',
+
+  // Bubble
+  bubbleBackground: 'White',
+  bubbleBorder: 'solid 1px #E6E6E6',
+  bubbleBorderRadius: 2,
+  bubbleFromUserBackground: 'White',
+  bubbleFromUserBorder: 'solid 1px #E6E6E6',
+  bubbleFromUserBorderRadius: 2,
+  bubbleFromUserTextColor: 'Black',
+  bubbleImageHeight: 240,
+  bubbleMaxWidth: 480, // screen width = 600px
+  bubbleMinHeight: 40,
+  bubbleMinWidth: 250, // min screen width = 300px, Edge requires 372px (https://developer.microsoft.com/en-us/microsoft-edge/platform/issues/13621468/)
+  bubbleTextColor: 'Black',
+
+  // Markdown
+  markdownRespectCRLF: true,
+
+  // Root
+  rootHeight: '100%',
+  rootWidth: '100%',
+
+  // Send box
+  hideSendBox: false,
+  hideUploadButton: false,
+  microphoneButtonColorOnDictate: '#F33',
+  sendBoxBackground: 'White',
+  sendBoxButtonColor: '#767676',
+  sendBoxButtonColorOnDisabled: '#CCC',
+  sendBoxButtonColorOnFocus: '#333',
+  sendBoxButtonColorOnHover: '#333',
+  sendBoxHeight: 40,
+  sendBoxMaxHeight: 200,
+  sendBoxTextColor: 'Black',
+  sendBoxBorderBottom: '',
+  sendBoxBorderLeft: '',
+  sendBoxBorderRight: '',
+  sendBoxBorderTop: 'solid 1px #E6E6E6',
+  sendBoxPlaceholderColor: '#767676',
+  sendBoxTextWrap: false,
+
+  // Visually show spoken text
+  showSpokenText: false,
+
+  // Suggested actions
+  suggestedActionBackground: 'White',
+  suggestedActionBorder: `solid 2px ${DEFAULT_ACCENT}`,
+  suggestedActionBorderRadius: 0,
+  suggestedActionImageHeight: 20,
+  suggestedActionTextColor: DEFAULT_ACCENT,
+  suggestedActionDisabledBackground: 'White',
+  suggestedActionDisabledBorder: `solid 2px #E6E6E6`,
+  suggestedActionDisabledTextColor: DEFAULT_SUBTLE,
+  suggestedActionHeight: 40,
+
+  // Timestamp
+  timestampColor: DEFAULT_SUBTLE,
+
+  // Transcript overlay buttons (e.g. carousel and suggested action flippers, scroll to bottom, etc.)
+  transcriptOverlayButtonBackground: 'rgba(0, 0, 0, .6)',
+  transcriptOverlayButtonBackgroundOnFocus: 'rgba(0, 0, 0, .8)',
+  transcriptOverlayButtonBackgroundOnHover: 'rgba(0, 0, 0, .8)',
+  transcriptOverlayButtonColor: 'White',
+  transcriptOverlayButtonColorOnFocus: 'White',
+  transcriptOverlayButtonColorOnHover: 'White',
+
+  // Video
+  videoHeight: 270, // based on bubbleMaxWidth, 480 / 16 * 9 = 270
+
+  // Connectivity UI
+  connectivityIconPadding: PADDING_REGULAR * 1.2,
+  connectivityMarginLeftRight: PADDING_REGULAR * 1.4,
+  connectivityMarginTopBottom: PADDING_REGULAR * 0.8,
+  connectivityTextSize: 12,
+  failedConnectivity: '#C50F1F',
+  slowConnectivity: '#EAA300',
+  notificationText: '#5E5E5E',
+
+  typingAnimationBackgroundImage: null,
+  typingAnimationHeight: 20,
+  typingAnimationWidth: 64,
+
+  spinnerAnimationBackgroundImage: null,
+  spinnerAnimationHeight: 16,
+  spinnerAnimationWidth: 16,
+  spinnerAnimationPaddingRight: 12
+};
+ */
+
 const styleOptions = {
+  // 04.a.display-images
   botAvatarImage: 'https://docs.microsoft.com/en-us/azure/bot-service/v4sdk/media/logo_bot.svg?view=azure-bot-service-4.0',
   userAvatarImage: 'https://github.com/compulim.png?size=64',
+  // 04.a.display-initials
+  botAvatarInitials: 'BF',
+  userAvatarInitials: 'WC',
+  // 05.a.branding
+  bubbleBackground: 'rgba(0, 0, 255, .1)',
+  bubbleFromUserBackground: 'rgba(0, 255, 0, .1)',
+  // 05.d.hide-upload-button-styling
+  hideUploadButton: true,
+
+  rootHeight: '400px'
 };
 
 const App: React.FC = () => {
@@ -27,14 +151,17 @@ const App: React.FC = () => {
 
       {!token
         ? <div>Getting token...</div>
-        : <WebChat
-          directLine={createDirectLine({
-            token,
-            websocket: false,
-          })}
-          styleOptions={styleOptions}
-          storeKey="webchat"
-        />}
+        : <div id="custom-webchat">
+<WebChat
+  directLine={createDirectLine({
+    secret: 'secret',
+    domain: 'http://localhost:3978/directline',
+    webSocket: false,
+  })}
+  styleOptions={styleOptions}
+  storeKey="webchat"
+/>
+        </div>}
     </div>
   );
 }
